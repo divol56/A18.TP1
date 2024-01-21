1fc549f0ae2fea3d39d08d54c05d09a43e890ce8
Amine Mohamed Saaidia <127903340+divol56@users.noreply.github.com>
2024-01-21 01:05:02 -0500

    modification servletHello

--- src/main/java/com/example/tp1_1/HelloServlet.java
+++ src/main/java/com/example/tp1_1/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h3>" +message+ "  " +  message+ "  "  + message + "</h3>");
         out.println("</body></html>");
     }
 
